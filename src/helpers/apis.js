8487e67dc9ce3f84012d9aea9fd7618dd382d4ab
Alex Laird <alexlaird@gmail.com>
2018-02-17 07:44:16 -0800

    Another React/Redux starter.

---
+++ src/helpers/apis.js
@@ -0,0 +1,4 @@
+import axios from "axios";
+
+// TODO: this should be parsed from the environment
+axios.defaults.baseURL = 'http://127.0.0.1:8000';
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App/App';
+import React from "react";
+import {render} from "react-dom";
+import {Provider} from "react-redux";
+import {store, configureFakeBackend} from "./helpers";
+import {App} from "./scenes/App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// setup fake backend
+configureFakeBackend();
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
