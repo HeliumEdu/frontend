eeb03add9e48bac7ca42c55d4128bfa3149f82b6
Alex Laird <alexdlaird@gmail.com>
2018-03-07 22:44:02 -0800

    Cleaning up webpack config to properly bundle JS and img/font assets (still not CSS).

--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,20 @@
 const path = require('path');
+const _ = require('lodash');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const NunjucksWebpackPlugin = require("nunjucks-webpack-plugin");
 
 const publicPath = "";
 const projectName = "Helium Student Planner";
 const projectTagline = "Lightening Your Course Load";
+const projectVersion = require("./package.json").version;
 
 const defaultContext = {
     PUBLIC_PATH: publicPath,
     PROJECT_NAME: projectName,
     PROJECT_TAGLINE: projectTagline,
     PROJECT_EMAIL: "contact@heliumedu.com",
-    PROJECT_VERSION: require("./package.json").version,
+    PROJECT_VERSION: projectVersion,
     COPYRIGHT_YEAR: new Date().getFullYear()
 };
 
@@ -22,17 +25,31 @@ module.exports = {
         'api': './assets/js/api.js'
     },
     output: {
-        filename: '[name].bundle.[chunkhash].js',
+        filename: path.join('js', `[name].${projectVersion}.bundle.js`),
         path: path.resolve(__dirname, 'dist'),
         publicPath: publicPath
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
+        new CopyWebpackPlugin([
+            {
+                from: '**/*.+(png|jpg|jpeg|gif|svg)'
+            }
+        ]),
+        new CopyWebpackPlugin([
+            {
+                from: '**/*.+(woff|otf|eot|tff)'
+            }
+        ]),
         new NunjucksWebpackPlugin({
             templates: [{
                 from: "src/templates/index.html",
                 to: "index.html",
-                context: defaultContext
+                context: _.extend(defaultContext, {
+                    'javascript': [
+                        path.join('js', `base.${projectVersion}.bundle.js`)
+                    ]
+                })
             }]
         })
     ]
