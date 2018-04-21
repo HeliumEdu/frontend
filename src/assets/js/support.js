0448f468af2aee7f3040fbf4ef320eac97792fa1
Alex Laird <alexdlaird@gmail.com>
2018-04-21 11:50:34 -0700

    no message

--- src/assets/js/support.js
+++ src/assets/js/support.js
@@ -7,31 +7,6 @@
  * @version 1.4.0
  */
 
-var AUTH_TOKEN = Cookies.get("authtoken");
-var CSRF_TOKEN = Cookies.get("csrftoken");
-
-// Initialize AJAX configuration
-function csrfSafeMethod(method) {
-    "use strict";
-
-    // These HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-}
-$.ajaxSetup({
-    beforeSend: function (xhr, settings) {
-        "use strict";
-
-        if (!csrfSafeMethod(settings.type)) {
-            // Send the token to same-origin, relative URLs only.
-            // Send the token only if the method warrants CSRF protection
-            // Using the CSRFToken value acquired earlier
-            xhr.setRequestHeader("X-CSRFToken", CSRF_TOKEN);
-        }
-        xhr.setRequestHeader("Authorization", AUTH_TOKEN !== undefined ? "Token " + AUTH_TOKEN : null);
-    },
-    contentType: "application/json; charset=UTF-8"
-});
-
 var SITE_HOST = location.host + "/";
 var SITE_URL = location.protocol + "//" + SITE_HOST;
 if (SITE_URL === "http://localhost:3000/" || SITE_URL === "http://127.0.0.1:3000/") {
