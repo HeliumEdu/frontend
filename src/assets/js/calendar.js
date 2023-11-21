ed52e852c23f3c90bb828e27689e65a8962d1ec4
Alex Laird <contact@alexlaird.com>
2023-11-21 15:19:20 -0600

    [heliumcli] Release 1.4.51

--- src/assets/js/calendar.js
+++ src/assets/js/calendar.js
@@ -1,12 +1,12 @@
 /**
- * Copyright (c) 2021 Helium Edu.
+ * Copyright (c) 2023 Helium Edu.
  *
  * JavaScript functionality for persistence and the HeliumCalendar object on the /planner/calendar page.
  *
  * FIXME: This implementation is pretty crude compared to modern standards and will be completely overhauled in favor of a framework once the open source migration is completed.
  *
  * @author Alex Laird
- * @version 1.4.47
+ * @version 1.4.51
  */
 
 /**
