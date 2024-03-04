916eb4fd81e0e53972775aa75eadd619f2e28ab6
Alex Laird <contact@alexlaird.com>
2024-03-03 21:42:41 -0600

    Update copyright years.

--- src/assets/js/verify.js
+++ src/assets/js/verify.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021 Helium Edu.
+ * Copyright (c) 2018, Helium Edu
  *
  * FIXME: This implementation is pretty crude compared to modern standards and will be completely overhauled in favor of a framework once the open source migration is completed.
  *
