03d0b92de5edbc8f447d2eacf86d932823e4f2c9
Alex Laird <contact@alexlaird.com>
2025-12-02 18:29:03 -0600

    Fix tests with shared deps

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,10 +9,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:helium_mobile/core/fcm_service.dart';
 import 'package:helium_mobile/main.dart';
+import 'package:shared_preferences_platform_interface/in_memory_shared_preferences_async.dart';
+import 'package:shared_preferences_platform_interface/shared_preferences_async_platform_interface.dart';
 
 import 'mock_firebase_setup.dart';
 
 void main() async {
+  setUpAll(() {
+    SharedPreferencesAsyncPlatform.instance =
+        InMemorySharedPreferencesAsync.withData({});
+  });
+
   await mockFirebaseInitialiseApp();
 
   group('HeliumEdu App Tests', () {
