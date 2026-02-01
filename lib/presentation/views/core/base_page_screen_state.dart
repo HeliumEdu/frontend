397ec51f07cb3091426b38e29392b89722515252
Alex Laird <contact@alexlaird.com>
2026-01-31 20:30:20 -0600

    Margin fixes to tighten things up.

--- lib/presentation/views/core/base_page_screen_state.dart
+++ lib/presentation/views/core/base_page_screen_state.dart
@@ -101,7 +101,7 @@ abstract class BasePageScreenState<T extends StatefulWidget> extends State<T> {
 
     // Build the main content
     final Widget content = Padding(
-      padding: const EdgeInsets.only(left: 12, right: 12, top: 12, bottom: 0),
+      padding: const EdgeInsets.only(left: 12, right: 12, top: 8, bottom: 0),
       child: Column(
         children: [
           if (isLoading)
