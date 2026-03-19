7b3bb01343b80b065aabba8d297e09f377808c6e
Alex Laird <contact@alexlaird.com>
2026-03-19 10:40:28 -0500

    Remove AppRoute import and URL replace calls
    Delete unused AppRoute imports and remove context.replace(...) calls that cleared query params after opening dialogs/screens in courses_screen.dart and notebook_screen.dart. This stops the code from programmatically replacing the current route (and removing query parameters) immediately after opening those UI flows.

--- lib/presentation/features/courses/views/courses_screen.dart
+++ lib/presentation/features/courses/views/courses_screen.dart
@@ -9,7 +9,6 @@ import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:heliumapp/config/app_route.dart';
 import 'package:heliumapp/config/app_theme.dart';
 import 'package:heliumapp/utils/color_helpers.dart';
 import 'package:heliumapp/core/dio_client.dart';
@@ -359,8 +358,6 @@ class _CoursesScreenState extends BasePageScreenState<_CoursesProvidedScreen> {
               isNew: false,
               initialStep: step,
             );
-            // Clear query params from URL after opening dialog/screen
-            context.replace(AppRoute.coursesScreen);
           });
         }
       }
