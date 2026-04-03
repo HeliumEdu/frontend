7e0b9bb1c799534028c7ad6b2ab90b62a6d783f5
Alex Laird <contact@alexlaird.com>
2026-04-03 16:41:07 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@
 // For details regarding the license, please refer to the LICENSE file.
 
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
